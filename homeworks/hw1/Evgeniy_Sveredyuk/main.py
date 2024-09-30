5b2c8c6edaa1b69548590f554ee74d6303bcddbf
CBeredIOk <CBeredIOk@yandex.ru>
2024-09-30 21:32:37 +0300

    Rename hw1.py to main.py and update it

--- homeworks/hw1/Evgeniy_Sveredyuk/main.py
+++ homeworks/hw1/Evgeniy_Sveredyuk/main.py
@@ -1,14 +1,11 @@
-import argparse
-
-import numpy as np
 import torch
+import argparse
 import pandas as pd
 import torch.nn as nn
 import torch.optim as optim
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, confusion_matrix
-
-BATCH_SIZE = 1024
 
 torch.manual_seed(42)
 
@@ -76,12 +73,12 @@ def evaluate(model, X, y):
     return predictions, accuracy, conf_matrix
 
 
-def train(model, criterion, optimizer, X_train, y_train, X_val, y_val, epochs):
+def train(model, criterion, optimizer, X_train, y_train, X_val, y_val, epochs, batch_size):
     for epoch in range(epochs+1):
         optimizer.zero_grad()
-        for i in range(0, X_train.size(0), BATCH_SIZE):
-            X_batch = X_train[i:i + BATCH_SIZE]
-            y_batch = y_train[i:i + BATCH_SIZE]
+        for i in range(0, X_train.size(0), batch_size):
+            X_batch = X_train[i:i + batch_size]
+            y_batch = y_train[i:i + batch_size]
             y = model(X_batch)
             loss = criterion(y, y_batch)
             loss.backward()
@@ -89,7 +86,7 @@ def train(model, criterion, optimizer, X_train, y_train, X_val, y_val, epochs):
 
         predictions, accuracy, conf_matrix = evaluate(model, X_val, y_val)
         if epoch % 10 == 0:
-            print(f'\nepoch: {epoch}\naccuracy: {accuracy}, conf_matrix: \n{conf_matrix}')
+            print(f'\nepoch: {epoch}\naccuracy: {accuracy},\nconf_matrix: \n{conf_matrix}')
     return model
 
 
@@ -100,10 +97,9 @@ def predict(X, model):
     return predictions
 
 
-def main():
+def main(args):
     # Load data
-    X_train, y_train, X_val, y_val, X_test = load_data(
-        '../data/train.csv', '../data/val.csv', '../data/test.csv')
+    X_train, y_train, X_val, y_val, X_test = load_data(args.train_csv, args.val_csv, args.test_csv)
 
     # Preprocess data
     X_train, y_train, X_val, y_val, X_test = preprocess_data(X_train, y_train, X_val, y_val, X_test)
@@ -112,7 +108,11 @@ def main():
     model, criterion, optimizer = init_model()
 
     # Train model
-    model = train(model, criterion, optimizer, X_train, y_train, X_val, y_val, 100)
+    model = train(
+        model, criterion, optimizer,
+        X_train, y_train, X_val, y_val,
+        args.num_epoches, args.batch_size
+    )
 
     # Predict on test set
     predictions = predict(X_test, model)
@@ -121,18 +121,16 @@ def main():
     pd.DataFrame(predictions).to_csv('submission.csv')
 
 
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#
-#     parser.add_argument('--train_csv', default='homeworks/hw1/data/train.csv')
-#     parser.add_argument('--val_csv', default='homeworks/hw1/data/val.csv')
-#     parser.add_argument('--test_csv', default='homeworks/hw1/data/test.csv')
-#     parser.add_argument('--out_csv', default='homeworks/hw1/data/submission.csv')
-#     parser.add_argument('--lr', default=0)
-#     parser.add_argument('--batch_size', default=0)
-#     parser.add_argument('--num_epoches', default=0)
-#
-#     args = parser.parse_args()
-#     main(args)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
-main()
+    parser.add_argument('--train_csv', default='../data/train.csv')
+    parser.add_argument('--val_csv', default='../data/val.csv')
+    parser.add_argument('--test_csv', default='../data/test.csv')
+    parser.add_argument('--out_csv', default='submission.csv')
+    parser.add_argument('--lr', default=0)
+    parser.add_argument('--batch_size', default=1024)
+    parser.add_argument('--num_epoches', default=100)
+
+    args = parser.parse_args()
+    main(args)
